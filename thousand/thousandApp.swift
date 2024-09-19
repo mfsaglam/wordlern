4be548dfa45404475d48b07b80e9c95799d9bbfc
mfsaglam <mfsaglam1@gmail.com>
2024-09-19 23:56:24 +0300

    Initial Commit

---
+++ thousand/thousandApp.swift
@@ -0,0 +1,17 @@
+//
+//  thousandApp.swift
+//  thousand
+//
+//  Created by Fatih Sağlam on 19.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct thousandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
